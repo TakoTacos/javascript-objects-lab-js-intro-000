3e7943afaae9bc1972a4a56c51091765d584aee9
Corey Lewis <clewis.development@gmail.com>
2018-05-15 05:20:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,3 +3,8 @@ var recipe = {key: "value"}
 function updateObjectWithKeyAndValue(recipe, key, value) {
   return Object.assign({}, {key: value})
 }
+
+function destructivelyUpdateObjectWithKeyAndValue(recipe, key, value) {
+  recipe[key] = value;
+  return recipe
+}
